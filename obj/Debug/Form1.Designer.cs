95ed496de78649dc6cb763e7469b4e3638c0a4f6
yassersadeghi <sadeghi.yasser@gmail.com>
2021-04-28 16:24:04 +0430

    Add files via upload

---
+++ obj/Debug/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace link_prediction_PSO_LA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.Font = new System.Drawing.Font("Palatino Linotype", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 35);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(696, 277);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(714, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "load graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "load by edge + year",
+            "load by freinds",
+            "load by edge"});
+            this.comboBox1.Location = new System.Drawing.Point(805, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Text = "load by edge + year";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 324);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ obj/Debug/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace link_prediction_PSO_LA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class freind
+        {
+            int index;
+            double  score;
+            int year;
+        }
+        public class node
+        {
+           public  int index;
+           public  int degree;
+           public   double weight;
+           public  freind[] freinds;
+           public node(int f)
+            {
+                degree = 0;
+                freinds = new freind[f] ;
+            }
+
+        }
+
+        public int loadfromfileEg(string filename,ref node[] nodes, int N )
+        {
+            nodes = new node[N];
+           
+
+            {
+                bool e = true;
+                int j = 1;
+                int n = 0;
+                FileStream f = File.OpenRead(filename);
+                TextReader r = new StreamReader(f);
+
+                while (e)
+                {
+                    string s = r.ReadLine();
+
+                    if ((s == null))   // end of file
+                    {
+                        e = false;
+                        break;
+                    }
+                    n++;
+                    s = s.Replace("\t", "            ") + "               ";
+                    bool check = true;
+                    int l = 0;
+                    int[] index = new int[10];
+
+                    for (int i = 0; i < s.Length; i++)    // counting numbers in each line of the file
+                    {
+                        if (s[i] == ' ')
+                            check = true;
+                        if (check & s[i] != ' ')
+                        {
+                            index[l++] = i;
+                            check = false;
+                        }
+                    }
+                    index[l] = s.Length + 1;
+
+                    //  for (int i = 0; i < index.Length - 1; i++)   // reading numbers
+                    //      if (index[i + 1] != 0)
+                    //      {
+                    int ind = Convert.ToInt32(s.Substring(index[0], (index[1] - index[0])).Trim());
+                    int ind2 = Convert.ToInt32(s.Substring(index[1], 8).Trim());
+
+                    
+
+                    if (!(nodes[ind].freinds.Contains((ind2))))
+                    {
+                        nodes[ind].degree++;
+                        
+                        nodes[ind].freinds[0] = ind;
+                        nodes[ind].freinds[nodes[ind].degree] = Convert.ToInt32(s.Substring(index[1], 8).Trim());
+                    }
+                    if (!(nodes[ind2].freinds.Contains(ind)))
+                    {
+                        nodes[ind2].degree++;
+                        nodes[ind2].freinds[0] = ind2;
+                        nodes[ind2].freinds[nodes[ind2].degree] = ind;
+
+                    }
+
+                    //      }
+
+                    j++;
+                }
+
+                r.Close();
+                f.Close();
+                return n;
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filename;
+            OpenFileDialog fd = new OpenFileDialog();
+            DialogResult dr= fd.ShowDialog();
+            if (dr.Equals(DialogResult.OK))
+                filename = fd.FileName;
+
+          //  Task T = new Task(new Action(loadfile))
+
+
+        }
+    }
+}
